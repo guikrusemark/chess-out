1926a5232fe967e3aff6be1143b2a42acdf3b080
guikrusemark <guilhermekrusemark@gmail.com>
2022-11-30 12:02:03 -0300

    Resolving

---
+++ doc/.template/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/asset_manager.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/asset_manager.cpp.o"
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/game.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/game.cpp.o"
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/game_state.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/game_state.cpp.o"
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/main.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/main.cpp.o"
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/pause_state.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/pause_state.cpp.o"
+  "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/src/state_machine.cpp" "/mnt/c/workspace/ufmg/PDS2-2022-2/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/src/state_machine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ doc/.template/SFML_Starter/build/CMakeFiles/SfmlStarter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SfmlStarter.dir/src/asset_manager.cpp.o"
+  "CMakeFiles/SfmlStarter.dir/src/game.cpp.o"
+  "CMakeFiles/SfmlStarter.dir/src/game_state.cpp.o"
+  "CMakeFiles/SfmlStarter.dir/src/main.cpp.o"
+  "CMakeFiles/SfmlStarter.dir/src/pause_state.cpp.o"
+  "CMakeFiles/SfmlStarter.dir/src/state_machine.cpp.o"
+  "SfmlStarter"
+  "SfmlStarter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SfmlStarter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
